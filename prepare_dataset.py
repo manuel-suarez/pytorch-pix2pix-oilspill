05b16222a307349e3125b8f17452eec463a064ea
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-17 12:32:37 -0600

    Tool for split the images in oil spill dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,12 +1,27 @@
 # The images in oil spill dataset have 1250x650 pixels so we must divide them and adjust to
 import os
 import config
+from PIL import Image
 
-def split_images(directory):
-    list_files = os.listdir(directory)
-    for f in list_files:
-        print(f)
+def split_images(origin, dest, prefix, ext):
+    os.makedirs(origin)
+    list_files = os.listdir(dest)
+    counter = 1
+    for fname in list_files:
+        im = Image.open(os.path.join(origin, fname))
+        # Left image
+        im1 = im.crop((1, 1, 600, 600))
+        seq = "{:04d}".format(counter)
+        im1.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
+        counter += 1
+        # Right image
+        im2 = im.crop((601, 0, 1200, 600))
+        seq = "{:04d}".format(counter)
+        im2.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
+        counter += 1
 
 # Split images
-split_images(os.path.join(config.TRAIN_DIR, 'images'))
+split_images('./dataset/images', os.path.join(config.TRAIN_DIR, 'images'), 'img', 'jpg')
 # Split labels
+#os.makedirs('./dataset/labels')
+#split_images(os.path.join(config.TRAIN_DIR, 'labels'))
