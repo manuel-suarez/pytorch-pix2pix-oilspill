be57ac7875c11394c56a75f447a30e247ae45585
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-20 19:19:52 -0600

    Iteration to crop subimages

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -9,18 +9,26 @@ def split_images(origin, dest, prefix, ext):
     list_files = os.listdir(origin)
     list_files.sort()
     counter = 1
+    step_x = (1200 - 256) // 9
+    step_y = (600 - 256) // 4
     for fname in tqdm(list_files):
         im = Image.open(os.path.join(origin, fname))
-        # Left image
-        im1 = im.crop((1, 1, 600, 600))
-        seq = "{:04d}".format(counter)
-        im1.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
-        counter += 1
+        # Crop subimages
+        for i in range(10):
+            for j in range(5):
+                dims = (i * step_x, j * step_y, i * step_x + 256, j * step_y + 256)
+                print(dims)
+                #ims = im.crop(dims)
+
+        #im1 = im.crop((1, 1, 600, 600))
+        #seq = "{:04d}".format(counter)
+        #im1.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
+        #counter += 1
         # Right image
-        im2 = im.crop((601, 0, 1200, 600))
-        seq = "{:04d}".format(counter)
-        im2.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
-        counter += 1
+        #im2 = im.crop((601, 0, 1200, 600))
+        #seq = "{:04d}".format(counter)
+        #im2.save(os.path.join(dest, f"{prefix}_{seq}.{ext}"))
+        #counter += 1
 
 def split_dataset(origin, dest):
     # Split images
