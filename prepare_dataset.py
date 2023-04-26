d5911c17c6c78bbdeff30747f133f626693b35d9
Arturo Suárez <manuel.suarez@cimat.mx>
2023-04-25 19:37:27 -0600

    Change subsample size to 64x64 (to use in DCGAN)

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,4 +1,4 @@
-# The images in oil spill dataset have 1250x650 pixels so we must divide them and adjust to
+# The images in oil spill dataset have 1250x650 pixels, so we must divide them and adjust to
 import os
 from config import TRAIN_DIR, TEST_DIR, VAL_DIR
 from PIL import Image
@@ -8,16 +8,17 @@ def split_images(origin, dest, prefix, ext):
     os.makedirs(dest)
     list_files = os.listdir(origin)
     list_files.sort()
-    step_x = (1200 - 256) // 3
-    step_y = (600 - 256) // 1
+    # Use steps of 64x64
+    step_x = 64 #(1200 - 256) // 3
+    step_y = 64 #(600 - 256) // 1
     for fname in tqdm(list_files):
         img_number = fname.split('.')[0].split('_')[1]
         im = Image.open(os.path.join(origin, fname))
         # Crop subimages
         counter = 1
-        for i in range(4):
-            for j in range(2):
-                dims = (i * step_x, j * step_y, i * step_x + 256, j * step_y + 256)
+        for i in range(1200 // 64):
+            for j in range(600 // 64):
+                dims = (i * step_x, j * step_y, i * step_x + 64, j * step_y + 64)
                 ims = im.crop(dims)
                 seq = "{:02d}".format(counter)
                 ims.save(os.path.join(dest, f"{prefix}_{img_number}_{seq}.{ext}"))
